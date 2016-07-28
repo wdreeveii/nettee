4f6c34bbc5f660c87a72b4b1722c422f085f22ea
Whitham D. Reeve II <thetawaves@gmail.com>
2016-07-28 11:09:17 -0800

    Initial commit

---
+++ client.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+type TeeClient struct {
+	address string
+	timeout time.Duration
+
+	in  chan []byte
+	out chan []byte
+
+	conn net.Conn
+	stop chan chan bool
+}
+
+func NewTeeClient(address string, timeout time.Duration, in chan []byte, out chan []byte) (*TeeClient, error) {
+	t := new(TeeClient)
+	t.stop = make(chan chan bool)
+	t.address = address
+	t.timeout = timeout
+	t.in = in
+	t.out = out
+
+	go t.handleConn()
+
+	return t, nil
+}
+
+func (t *TeeClient) dial() {
+	fmt.Println("dial..")
+	var err error
+
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	for err != nil {
+		fmt.Println(err)
+
+		time.Sleep(1 * time.Second)
+
+		t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+		fmt.Println("retry", err)
+	}
+
+	fmt.Println("final", err)
+
+}
+
+func (t *TeeClient) handleConn() {
+	t.dial()
+
+	r := bufio.NewReader(t.conn)
+
+	for {
+		select {
+		case done := <-t.stop:
+			done <- true
+			t.conn.Close()
+			return
+		case output := <-t.out:
+			t.conn.Write(output)
+		default:
+			var data []byte
+			t.conn.SetDeadline(time.Now().Add(t.timeout))
+			c, err := r.ReadByte()
+			data = append(data, c)
+			if err != nil {
+				fmt.Println(err)
+				t.conn.Close()
+				t.dial()
+			}
+			t.in <- data
+		}
+	}
+}
+
+func (t *TeeClient) Close() {
+	done := make(chan bool)
+	t.stop <- done
+	<-done
+	return
+}
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	in := make(chan []byte, 100)
+	out := make(chan []byte)
+
+	go func(src chan []byte) {
+		for {
+			data := <-src
+			fmt.Println(string(data))
+		}
+	}(in)
+
+	c, err := NewTeeClient("sadc_ts9:4039", 5*time.Second, in, out)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	time.Sleep(10 * time.Minute)
+
+	c.Close()
+}
